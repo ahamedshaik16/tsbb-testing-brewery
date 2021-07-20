6f0b496bce83b6fc274e7252f8bffc6913c7d2e5
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-20 16:01:58 +0530

    Initial SetUp

---
+++ src/main/java/com/sohail/tsbbtestingbrewery/TsbbTestingBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.sohail.tsbbtestingbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsbbTestingBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsbbTestingBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sohail/tsbbtestingbrewery/TsbbTestingBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohail.tsbbtestingbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsbbTestingBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
